7f20d485a59c8a9f3ecbf2b91298c6d9e171706e
Shruti Arora <144051611+shrutiarora9@users.noreply.github.com>
2025-12-01 08:02:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-
 import chatbot_logic
 import os
 
 app = Flask(__name__)
 CORS(app)
-
 
 @app.route("/", methods=["GET"])
 def home():
@@ -21,6 +19,4 @@ def chat():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
-
-
+    app.run(host="0.0.0.0", port=port)

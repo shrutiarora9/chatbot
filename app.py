70f655702f63d396fbbee9a885dd790df0c32465
Shruti Arora <144051611+shrutiarora9@users.noreply.github.com>
2025-12-01 07:53:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,6 +20,7 @@ def chat():
     return jsonify({"reply": reply})
 
 if __name__ == "__main__":
-     port = int(os.environ.get("PORT", 10000))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
+
 
